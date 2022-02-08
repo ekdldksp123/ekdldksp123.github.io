ae97a5f731e4438967d871ee799195b266243820
ekdldksp123 <ekdldksp123@naver.com>
2022-02-09 00:36:07 +0900

    Deploy Next.js to gh-pages

---
+++ out/_next/static/BJ8wAJ_Za2T4NhBKeFyix/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/141-0ec5c4205f81c3c4.js","static/chunks/pages/index-89b13b3ef8b87d52.js"],"/_error":["static/chunks/pages/_error-6679c1efb053c50e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
