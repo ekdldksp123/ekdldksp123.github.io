2b29981f06253b546947633540dd1d98c3069615
ekdldksp123 <64599394+ekdldksp123@users.noreply.github.com>
2022-02-14 14:08:22 +0000

    Deploying to gh-pages from @ ekdldksp123/ekdldksp123.github.io@6afa7fa86b6346db13516513f4861b3e6d009531 🚀

--- _next/static/20YSzKHJCO41F-S-wht5R/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/596-1b1670cf9a6dc7de.js","static/chunks/pages/index-779ebb9887406901.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
