3b3d747e588511501d09928eeeea19214093e3a3
ekdldksp123 <64599394+ekdldksp123@users.noreply.github.com>
2023-03-05 15:04:29 +0000

    Deploying to gh-pages from @ ekdldksp123/ekdldksp123.github.io@dee1861bcffc683159a61637fe173e933d24e727 🚀

--- styles/config.ts
+++ styles/config.ts
@@ -1,32 +1,31 @@
 export const scripts = [
-    "https://cdnjs.cloudflare.com/ajax/libs/parallax/3.1.0/parallax.min.js",
-    "https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.1/js/swiper.min.js",
-    "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js",
-    "../js/index.js"
+  "https://cdnjs.cloudflare.com/ajax/libs/parallax/3.1.0/parallax.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.1/js/swiper.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js",
+  "../js/index.js",
 ];
 
-const swiper = 'https://unpkg.com/swiper/swiper-bundle.min.css';
+export const swiper = "https://unpkg.com/swiper/swiper-bundle.min.css";
 
 export const init = () => {
-
-    const link = document.createElement('link');
-    link.setAttribute('rel', 'stylesheet');
-    link.setAttribute('href', swiper);
-    document.head.appendChild(link);
+  const link = document.createElement("link");
+  link.setAttribute("rel", "stylesheet");
+  link.setAttribute("href", swiper);
+  document.head.appendChild(link);
 };
 
 export const globalCss = `
 ::-webkit-scrollbar {
-    width: 10px; 
+    width: 10px;
 }
 ::-webkit-scrollbar-track {
-    background: transparent; 
-    border-radius: 10px; 
+    background: transparent;
+    border-radius: 10px;
 }
 ::-webkit-scrollbar-thumb {
-    height: 5%; 
-    background: linear-gradient(#c2e59c, #64b3f4); 
-    border-radius: 10px; 
+    height: 5%;
+    background: linear-gradient(#c2e59c, #64b3f4);
+    border-radius: 10px;
 }
 
 body {
